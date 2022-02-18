94639a6758ef70a58f31d2911ec2f394527210d8
pokerty <chenhongpo@gmail.com>
2022-02-18 16:55:52 +0800

    added working dijkstra and incomplete bfs(no cost)

--- Python/bfs.py
+++ Python/bfs.py
@@ -3,6 +3,7 @@
 # traverses vertices reachable from s.
 from collections import defaultdict
 import json
+ 
 # This class represents a directed graph
 # using adjacency list representation
 class Graph:
@@ -17,8 +18,21 @@ class Graph:
     def addEdge(self,u,v):
         self.graph[u].append(v)
  
+    def backtrace(self, parent, start, end):
+        path = [end]
+        while path[-1] != start:
+            path.append(parent[path[-1]])
+        for i in path:
+            print("old path is", i, end="->")
+        path.reverse()
+        for i in path:
+            print("path is", i, end="->")
+        return path
+
     # Function to print a BFS of graph
-    def BFS(self, s):
+    def BFS(self, s, d):
+        # dict for parent node
+        parent = {}
  
         # Mark all the vertices as not visited
         visited = [False] * (max(self.graph) + 1)
@@ -28,27 +42,43 @@ class Graph:
  
         # Mark the source node as
         # visited and enqueue it
-        queue.append(s)
-        visited[s] = True
+        cur = s;  #mark current node
+        queue.append(cur)
+        visited[cur] = True
  
         while queue:
  
             # Dequeue a vertex from
             # queue and print it
-            s = queue.pop(0)
-            print (s, end = " ")
+            cur = queue.pop(0)
+            # print("it is traversing", cur) #test
+            # print (cur, end = " ")
+            if cur == d: #if dest found
+                path = []
+                while cur!=s: #backtrace while parent not source
+                    path.append(cur)
+                    cur=parent[cur]
+                # path.append(s) #add source
+                path.reverse() #reverse
+                print(s, end=" ") #start
+                for i in path:
+                    print("->", i, end=" ")
+                # self.backtrace(parent, s, d)
+                return 
  
             # Get all adjacent vertices of the
             # dequeued vertex s. If a adjacent
             # has not been visited, then mark it
             # visited and enqueue it
-            for i in self.graph[s]:
+            for i in self.graph[cur]:
                 if visited[i] == False:
+                    parent[i] = cur #add parent of cur node to dict
+                    # print ("parent of", i  ,"is", parent[i]) #test
                     queue.append(i)
                     visited[i] = True
  
 # Driver code
- 
+
 # Create a graph given in
 # the above diagram
 G = open('G.json')
@@ -67,19 +97,20 @@ costdata = json.load(Cost)
 graph = Graph()
 # GRAPH PLUS 1 BECAUSE IT STARTS FROM 1 INSTEAD OF 0
 
-for i in distdata:
+for i in costdata:
     j = i.split(',')
-    graph.addEdge(int(j[0]), int(j[1]), float(distdata[i]))
+    graph.addEdge(int(j[0]), int(j[1]))
+    # graph.addEdge(int(j[0]), int(j[1]), int(costdata[i]))
 # EVERY NODE INDEX WILL MINUS 1
 
-graph.BFS(2)
+print ("The breadth first traversal path for 1 to 50 is")
+graph.BFS(1, 50)
 # Closing file
 G.close()
 Dist.close()
 Cost.close()
  
-print ("Following is Breadth First Traversal"
-                  " (starting from vertex 2)")
+
 
  
 # This code is contributed by Neelam Yadav
