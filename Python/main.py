786f07e242ba12f093f698ba5921fa2dcd307f87
wxpei <wxpei_7699@hotmail.com>
2022-03-07 18:16:12 +0800

    Create main.py

---
+++ Python/main.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+#import sys
+import json
+import time 
+import heapq
+import math
+
+print('---Task1---')
+import task1
+
+print()
+print('---Task2---')
+import task2
+
+print()
+print('---Task3---')
+import task3
